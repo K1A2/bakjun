62b0ad9a2f3582ba0af7216e59fc4d85490ea6ff
K1A2 <jckim0307@gmail.com>
2021-05-15 23:13:11 +0900

    3

--- 2447_별찍기_재귀_10.py
+++ 2447_별찍기_재귀_10.py
@@ -1,6 +1,19 @@
-def print_star():
-    l = ['', '', '']
-    for i in range(3):
-        l[i] += print_star()
-i = int(input())
-print(print_star())
+def print_star(num):
+    if num == 3:
+        return ['***', '* *', '***']
+    else:
+        la = ['' for _ in range(num)]
+        for i in range(3):
+            for j in range(3):
+                if i == 1 and j == 1:
+                    for m in range(num // 3):
+                        la[m + num // 3] += ' ' * (num // 3)
+                else:
+                    n = print_star(num // 3)
+                    for m in range(len(n)):
+                        la[m + i * (num // 3)] += n[m]
+        return la
+
+answer = print_star(int(input()))
+for i in answer:
+    print(i)
